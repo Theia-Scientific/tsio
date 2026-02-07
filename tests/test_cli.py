e3be5d072c273ad37e765e1eca3338fa228361aa
Christopher Field <chris.field@theiascientific.com>
2026-02-07 09:48:00 -0500

    Add tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,10 +3,34 @@
 import importlib.metadata
 
 from tsio import __app_name__
-from tsio.cli import app, map_verbosity
+from tsio.cli import (
+    app,
+    map_verbosity,
+    JPEG_FILE_EXT,
+    JPEG_MIME_TYPE,
+    OutputFileFormats,
+    PNG_FILE_EXT,
+    PNG_MIME_TYPE,
+    TIFF_FILE_EXT,
+    TIFF_MIME_TYPE,
+    write,
+)
 from typer.testing import CliRunner
 
 runner = CliRunner()
+
+
+def test_output_file_formats_mime_type():
+    assert OutputFileFormats.JPEG.mime_type == JPEG_MIME_TYPE
+    assert OutputFileFormats.PNG.mime_type == PNG_MIME_TYPE
+    assert OutputFileFormats.TIFF.mime_type == TIFF_MIME_TYPE
+
+
+def test_output_file_formats_file_ext():
+    assert OutputFileFormats.JPEG.file_ext == JPEG_FILE_EXT
+    assert OutputFileFormats.PNG.file_ext == PNG_FILE_EXT
+    assert OutputFileFormats.TIFF.file_ext == TIFF_FILE_EXT
+
 
 def test_map_verbosity_false():
     actual = map_verbosity(False)
@@ -16,6 +40,13 @@ def test_map_verbosity_false():
 def test_map_verbosity_true():
     actual = map_verbosity(True)
     assert actual == "DEBUG"
+
+
+# def test_write(tmp_path):
+#     src = tmp_path.joinpath("test.tif")
+#     dst = src.with_suffix(JPEG_FILE_EXT)
+#     write(reader, src, None, OutputFileFormats.JPEG, True, False)
+#     assert dst.exists()
 
 
 def test_app_help():
