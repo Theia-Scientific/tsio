566a78958b29aca9a449d459bc1a3170e3491968
Christopher Field <chris.field@theiascientific.com>
2026-08-26 17:20:31 -0400

    Fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -491,8 +491,9 @@ def test_write_emd_with_no_image_data(mocker, tmp_path):
     mocker.patch("rsciio.emd.file_reader", mock_file_reader)
     write_emd(
         Configuration(
-            dst=None,
-            output_format=OutputFileFormats.JPEG,
+            output=Output(
+                bit_depth=BitDepths.EIGHT, format=OutputFileFormats.JPEG, path=None
+            ),
             silent=True,
             src=src,
         )
